09069b5064f868859e0f74d9ca2f35bb887c2594
Genetical <18619788+Genetical@users.noreply.github.com>
2021-03-05 16:37:48 +0000

    Remove secret keys from source

--- Spitfire1/config.py
+++ Spitfire1/config.py
@@ -1,11 +1,14 @@
+import os
+
+
 class Config:
-    SECRET_KEY = "136857bafd97dfce1f0c2bf8e4e620a3"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "136857bafd97dfce1f0c2bf8e4e620a3")
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = "spitfireappemail@gmail.com"
-    MAIL_PASSWORD = "HyO!vBS77d*6j^qWM0"
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     UPLOAD_FOLDER = "static/group_files"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
